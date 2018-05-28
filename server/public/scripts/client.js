dab2f6756650628f58a1c46f37704a151999d938
Ben Hacker <bhacker1995@gmail.com>
2018-05-28 10:55:58 -0500

    Calculator displays inputs

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,8 +6,26 @@ class Calculation{
   } //end constructor
 } // end class
 
+let inputArr = [];
+let inputVal = $( '#input' );
+let output = [];
+
 $( document ).ready( readyNow );
 
 function readyNow(){
+  inputArr = [];
+  inputVal.val( '' );
+  $( '#input' ).prop( 'readonly', true );
   console.log( 'JQ' );
+  // $( '#1Button' ).on( 'click', Press1 );
+  $(".input").click( appendInput );
+    
 } //end readyNow
+
+function appendInput() {
+  let number = $(this).data( 'number' );
+    $("#input").val( function() {
+        return this.value + number;
+    });
+  }
+
